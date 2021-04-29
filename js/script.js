5f74f27a5cd5f0e4e2c7d39caa849e20100e6526
ismaelmarin2010 <ismaelmarin2010@gmail.com>
2021-04-29 00:15:00 -0300

    28/04/2021

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
 var lastUpdated = document.lastModified;
 document.getElementById("date").innerHTML = lastUpdated;
 
-var y = new Date();
+/*var y = new Date();
 var fullYear = y.getFullYear();
-document.getElementById("year").innerHTML = fullYear;
+document.getElementById("year").innerHTML = fullYear;*/
+
+var y = new Date();
+document.querySelector("#year").innerText = y.getFullYear();
