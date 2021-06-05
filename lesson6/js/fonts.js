f6df6b8dc75fe390f1d698d66f3cb707b8673ee3
ismaelmarin2010 <ismaelmarin2010@gmail.com>
2021-06-05 19:50:26 -0300

    Enter

--- lesson6/js/fonts.js
+++ lesson6/js/fonts.js
@@ -1,5 +1,5 @@
-WebFontConfig = {
+WebFont.load ({
     google: {
-        families: ['Open Sans:Semi-bold 600', 'Roboto Condensed:Light 300']
+        families: ['Open Sans:Semi-Bold 600','Roboto Condensed:Light 300']
     } 
-};
+});
