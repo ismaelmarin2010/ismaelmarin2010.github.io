7868725a07610743b254a695b74e81136bd77fe8
ismaelmarin2010 <ismaelmarin2010@gmail.com>
2021-06-05 22:18:15 -0300

    Enter

---
+++ lesson7/js/lazyload.js
@@ -0,0 +1,7 @@
+src="https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js"
+
+WebFontConfig = {
+    google: {
+        families: ['Open Sans:Semi-bold 600', 'Roboto Condensed:Light 300']
+    } 
+};
