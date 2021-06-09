27554ba660c1669596077a46be6d2e51108cdd0b
ismaelmarin2010 <ismaelmarin2010@gmail.com>
2021-06-09 14:38:46 -0300

    Enter

--- lesson7/js/script.js
+++ lesson7/js/script.js
@@ -76,3 +76,30 @@ if (days[dayNumber] == "Friday") {
     document.querySelector('#banner').classList.toggle("hide", false);
     document.querySelector('#banner').classList.toggle("banner", true);
 }
+
+
+// Progressive loading images
+const imagesToLoad = document.querySelectorAll('img[data-src]');
+const loadImages = (image) => {
+  image.setAttribute('src', image.getAttribute('data-src'));
+  image.onload = () => {
+    image.removeAttribute('data-src');
+  };
+};
+if ('IntersectionObserver' in window) {
+  const observer = new IntersectionObserver((items) => {
+    items.forEach((item) => {
+      if (item.isIntersecting) {
+        loadImages(item.target);
+        observer.unobserve(item.target);
+      }
+    });
+  });
+  imagesToLoad.forEach((img) => {
+    observer.observe(img);
+  });
+} else {
+  imagesToLoad.forEach((img) => {
+    loadImages(img);
+  });
+}
