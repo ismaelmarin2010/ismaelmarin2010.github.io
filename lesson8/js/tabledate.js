f3fed5d94b8435fc4bf9ab1be5199bf760911161
ismaelmarin2010 <ismaelmarin2010@gmail.com>
2021-06-12 03:37:22 -0300

    Enter

--- lesson8/js/tabledate.js
+++ lesson8/js/tabledate.js
@@ -1,8 +1,8 @@
 var date = new Date();
 
-var day = date.getDay()
+var day = date.getDate()
 var month = date.getMonth()
 var year = date.getFullYear()
 
-credits = `Compiled ${month}.${day}.${year} by IsmaelM`;
+credits = `${day}`//`Compiled ${month}.${day}.${year} by IsmaelM`;
 document.querySelector('#credits').textContent = credits;
